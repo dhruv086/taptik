c3f368d1dc59e6aff95a4f330d607812760ce029
dhruv086 <dhruvagarwal086@gmail.com>
2025-06-19 17:12:12 +0530

    deploying the website

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,16 @@ import connectDB from "./lib/db.js"
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import { app,server } from "./lib/socket.js"
+import path from "path"
+
 
 dotenv.config()
 
 
 import authRoute from "./routes/auth.route.js"
 import messageRoute from "./routes/message.route.js"
+
+const __dirname = path.resolve()
 
 
 app.use(express.json({limit:'10mb'}))
@@ -24,6 +28,14 @@ app.use(cors({
 
 app.use("/api/auth",authRoute)
 app.use("/api/messages",messageRoute)
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend", "dist","index.html"));
+  }); 
+}
 
 server.listen(process.env.PORT,()=>{
   console.log("server is running",process.env.PORT)
