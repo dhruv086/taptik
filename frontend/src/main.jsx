3e9a4191ad31323f1c15a8ddda9236629be9e3ea
dhruv086 <dhruvagarwal086@gmail.com>
2025-06-23 18:54:39 +0530

    feat: wrap App component with VideoCallProvider for video call context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./App.css"; // Import Tailwind CSS
+import VideoCallProvider from "./components/VideoCallProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <VideoCallProvider>
+        <App />
+      </VideoCallProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
